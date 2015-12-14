7f5214c95cc0d653d7623d75d14be4b8eaa13892
NoMemo <Wing.Of.War.1980th@Gmail.com>
2015-12-15 00:00:28 +0800

    nim-game

---
+++ LeetCode_C/Leetcode/nim-game.c
@@ -0,0 +1,14 @@
+//
+//  nim-game.c
+//  LeetCode_C
+//
+//  Created by JUE DUKE on 15/12/14.
+//  Copyright © 2015年 JUE DUKE. All rights reserved.
+//
+
+#include "nim-game.h"
+
+
+int canWinNim(int n) {
+    return n % 4;
+}
