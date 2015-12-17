cd43527d5598cae911103ec3fdf1967c45f62460
NoMemo <Wing.Of.War.1980th@Gmail.com>
2015-12-17 23:54:21 +0800

    UGLY NUMBER EXCEL SHEET COLUMN NUMBER

---
+++ LeetCode_C/Leetcode-Easy/excel-sheet-column-title.c
@@ -0,0 +1,9 @@
+//
+//  excel-sheet-column-title.c
+//  LeetCode_C
+//
+//  Created by JUE DUKE on 15/12/15.
+//  Copyright © 2015年 JUE DUKE. All rights reserved.
+//
+
+#include "excel-sheet-column-title.h"
--- LeetCode_C/main.c
+++ LeetCode_C/main.c
@@ -7,7 +7,7 @@
 //
 
 #include <stdio.h>
-
+#include <stdbool.h>
 #include "linked-list.h"
 
 #pragma mark - Leetcode
@@ -19,10 +19,49 @@
 #include "string.h"
 //#include "median-of-two-sorted-arrays.h"
 #include "move-zeroes.h"
+
+
+
+bool isAnagram(char* s, char* t) {
+    if (s == NULL && t == NULL ){
+        return true;
+    }
+    
+    size_t l1 = strlen(s);
+    size_t l2 = strlen(t);
+    
+    if (l1 == 0 && l2 == 0) {
+        return true;
+    }
+    if (l1 != l2) {
+        return false;
+    }
+    char result[l1];
+    for (int i = 0 ; i < l1; i++) {
+        for (int j = 0; j < l2; j++) {
+            if (s[i] == t[j]) {
+                result[j] = '1';
+                break;
+            }
+        }
+    }
+    
+    for (int i = 0; i < l2; i++) {
+        if (result[i] != '1') {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
 int main(int argc, const char * argv[]) {
     
     //    linklist_test();
+    char *s = "anagram";
+    char *t = "nagaram";
     
+    isAnagram(s, t);
     
 #pragma mark - Leetcode
     //    testZigzag();
